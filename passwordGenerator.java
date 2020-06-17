291b59f9bed24db2d27294e50fe6ebfce2b49127
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-06-18 01:05:33 +0530

    FINAL

---
+++ passwordGenerator.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class passwordGenerator {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the length of the password");
+        int length = in.nextInt();
+        System.out.println(getPassword(length));
+        in.close();
+    }
+
+    private static String getPassword(int length) {
+
+        String number = "0123456789";
+        String lowerCase = "qazwsxedcrfvtgbyhnujmiklop";
+        String upperCase = "QWERTYUIOPLMKNJBHVGCFXDZSA";
+        String specialSymbols = "!@$#";
+        String combineCharacter = lowerCase;
+
+        Random rand = new Random();
+        System.out.println("Do you want to include Uppercase alphabets (y/n)?");
+        char c = in.next().charAt(0);
+        if (c == 'y') {
+            combineCharacter += upperCase;
+        }
+        System.out.println("Do you want to include number (y/n)?");
+        c = in.next().charAt(0);
+        if (c == 'y') {
+            combineCharacter += number;
+        }
+        System.out.println("Do you want to include Special Symbols (y/n)?");
+        c = in.next().charAt(0);
+        if (c == 'y') {
+            combineCharacter += specialSymbols;
+        }
+
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            password[i] = combineCharacter.charAt(rand.nextInt(combineCharacter.length()));
+        }
+
+        return new String(password);
+    }
+}
